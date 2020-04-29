28cc32293f0cb5e1da2228ed491e8cd51263d998
wisepythagoras <55108282+wisepythagoras@users.noreply.github.com>
2020-04-28 21:47:18 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import RichEditor from './RichEditor';
+import './styles.css';
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <RichEditor />
+        </div>
+    );
+}
---
+++ src/RichEditor.tsx
@@ -0,0 +1,223 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const EditorWrapper = styled.div`
+    border-radius: 5px;
+    padding: 15px;
+    border: 1px solid #fafafa;
+    display: grid;
+    grid-template-rows: 150px 1fr;
+    max-width: 1000px;
+
+    &:hover {
+        border: 1px solid #f0f0f0;
+    }
+`;
+
+const GhostEditor = styled.textarea`
+    color: transparent;
+    font-family: inherit;
+    font-size: 14px;
+    caret-color: #333;
+    resize: none;
+    border: none;
+    width: 100%;
+    max-width: inherit;
+    grid-area: 1 / 1 / 2 / 2;
+    word-break: break-all;
+    outline: 0;
+`;
+
+const DisplayBlock = styled.div`
+    background-color: transparent;
+    font-size: 14px;
+    padding-left: 1px;
+    grid-area: 1 / 1 / 2 / 2;
+    width: 100%;
+    max-width: inherit;
+    word-break: break-all;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+     -khtml-user-select: none;
+       -moz-user-select: none;
+        -ms-user-select: none;
+            user-select: none;
+`;
+
+interface REProps {
+    defaultValue?: string;
+}
+
+interface REState {
+    value: string;
+    richValue: JSX.Element|null;
+    textWidth: number;
+}
+
+const getTextWidth = (text: string, font: string) => {
+    const canvas: any = document.createElement('canvas');
+    const context: any = canvas.getContext('2d');
+
+    context.font = font || getComputedStyle(document.body).font;
+
+    return context.measureText(text).width;
+};
+
+const parseDimension = (str: string) => {
+    if (str) {
+        const tempWidth = str.match(/(\d+)px$/);
+
+        if (tempWidth && tempWidth.length > 1) {
+            return parseInt(tempWidth[1], 10) || 1;
+        }
+    }
+
+    return 1;
+};
+
+class RichEditor extends React.Component<REProps, REState> {
+    textInput: any;
+
+    /**
+     * Create a new RichEditor.
+     * @param props The components props.
+     */
+    constructor(props: REProps) {
+        super(props);
+
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+
+        this.textInput = React.createRef();
+
+        this.state = {
+            value: props.defaultValue || '',
+            richValue: null,
+            textWidth: 0,
+        };
+    }
+
+    /**
+     * Parse the contents in the string and return a JSX.Element type with the
+     * rich content.
+     * @returns {JSX.Element}
+     */
+    _parseContent(text: string) : JSX.Element {
+        return (
+            <span>{text.split(' ').map((word: string) => {
+                // Check for a hashtag.
+                const foundHashtag = word.match(/^#(\w+)([!.,/;'"?]*)$/);
+
+                if (foundHashtag && foundHashtag.length > 1) {
+                    return (
+                        <span>
+                            <a href={`/hashtag/${foundHashtag[1]}`}>
+                                #{foundHashtag[1]}
+                            </a>
+                            {foundHashtag.length == 3 ? foundHashtag[2] : ''}&nbsp;
+                        </span>
+                    );
+                }
+
+                // Now check for a user.
+                const foundUser = word.match(/^@([a-zA-Z0-9\-\_]+)([!.,/;'"?]*)$/);
+
+                if (foundUser && foundUser.length > 1) {
+                    return (
+                        <span>
+                            <a href={`/${foundUser[1]}`}>
+                                @{foundUser[1]}
+                            </a>
+                            {foundUser.length == 3 ? foundUser[2] : ''}&nbsp;
+                        </span>
+                    );
+                }
+
+                return `${word} `;
+            })}</span>
+        );
+    }
+
+    /**
+     * When the input value changes.
+     * @param e The change event.
+     */
+    onChange(e: any) {
+        const value = e.target.value;
+        const style = getComputedStyle(this.textInput.current);
+        const font = `${style.fontSize} ${style.fontFamily}`;
+        const textWidth = getTextWidth(value, font);
+
+        this.setState({
+            value,
+            richValue: this._parseContent(value),
+            textWidth,
+        });
+    }
+
+    /**
+     * When the display div is clicked.
+     * @param e The change event.
+     */
+    onClick(e: any) {
+        const style = getComputedStyle(this.textInput.current);
+        const styleWidth: number = parseDimension(style.width);
+        const styleHeight: number = parseDimension(style.height);
+
+        // Get the number of rows in the textarea.
+        const rows = Math.ceil(this.state.textWidth / styleWidth);
+
+        // We need the approximate width of a character in a string.
+        const charWidth = !!this.state.value ?
+            this.state.textWidth / this.state.value.length :
+            1;
+
+        // Same for the char height;
+        const charHeight = parseDimension(style.fontSize) + 2;
+
+        // This will show us how many characters we can have on a single row.
+        const charPerRow = Math.floor(styleWidth / charWidth);
+        console.log('[INFO]: charsPerRow', charPerRow);
+
+        const { offsetTop, offsetLeft } = this.textInput.current;
+        const clickX = e.pageX - offsetLeft;
+        const clickY = e.pageY - offsetTop;
+        console.log('[CLICK]:', clickX, clickY);
+        
+        const clickCol = Math.round(clickX / charWidth);
+        const clickRow = Math.floor(clickY / charHeight);
+        console.log('[INFO]', clickCol, clickRow);
+
+        const selectionEnd = clickCol + clickRow * charPerRow;
+
+        this.textInput.current.focus();
+        this.textInput.current.selectionStart = selectionEnd;
+        this.textInput.current.selectionEnd = selectionEnd;
+    }
+
+    /**
+     * Render the components.
+     */
+    render(): JSX.Element {
+        return (
+            <EditorWrapper>
+                <GhostEditor
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    ref={this.textInput}
+                />
+                <DisplayBlock onClick={this.onClick}>
+                    {this.state.richValue}
+                </DisplayBlock>
+                {/* <div style={{
+                    height: `20px`,
+                    width: `${this.state.textWidth}px`,
+                    background: 'red',
+                }} /> */}
+            </EditorWrapper>
+        );
+    }
+}
+
+export default RichEditor;
+
